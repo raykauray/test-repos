cf12c298b20c8e506af0d59c52cfd5ac7cdf233a
Zaheer ud Din Faiz <zaheerfazy@gmail.com>
2020-12-27 16:51:10 +0500

    Added dynamic python command name according to OS If OS is windows. It is "python" else for linux or mac it is "python3"

--- src/server/pythonScriptUtil.js
+++ src/server/pythonScriptUtil.js
@@ -1,20 +1,33 @@
 const { spawn } = require("child_process");
 const GLOBAL = require("../global/global.json");
 const path = require("path");
+const os = require("os");
+function getPythonNameByOS() {
+  let thisOS = os.platform();
+  if (thisOS === "win32") return "python";
+  if (thisOS === "linux" || thisOS === "darwin") return "python3";
+}
+
 function runPythonScripts(inputYamlFile) {
   return new Promise((resolve, reject) => {
-    
-    const command = `python3 ${path.join(
+    let pythonName = getPythonNameByOS();
+    let pythonScriptResponse = "";
+    const command = `${pythonName} ${path.join(
       __dirname,
       GLOBAL.pathToYamlValidotorPython
-    )} ${path.join(__dirname, inputYamlFile)}`;
-    
+    )} ${inputYamlFile}`;
+
     const childProcess = spawn(command, { shell: true });
+
     childProcess.stdout.on("data", (stdOutput) => {
-      resolve(stdOutput.toString());
+      pythonScriptResponse += stdOutput.toString();
     });
+
     childProcess.on("error", (error) => {
       console.error(error.toString());
+    });
+    childProcess.on("exit", (code) => {
+      resolve(pythonScriptResponse);
     });
     childProcess.stderr.on("data", (stdError) => {
       console.error(stdError.toString());
